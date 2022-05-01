3c6c396b9440e7c42811a9e3ccdbdc5afdaf59a8
evgesha1247 <dnsbtrq25@gmail.com>
2022-05-01 23:26:29 +0300

    наладил архетектуру (сомог отделить json)

--- lib/domain/data/timetable_data.dart
+++ lib/domain/data/timetable_data.dart
@@ -3,8 +3,8 @@ import 'package:flutter_hive_json/domain/entity/timetable.dart';
 
 class TimetableData {
   Timetable timetable = Timetable(posts: []);
-  Future<void> load() async {
+  Future<Timetable> load() async {
     ApiClient apiClient = ApiClient();
-    timetable = Timetable(posts: await apiClient.getPostsFormNetwork());
+    return Timetable(posts: await apiClient.getPostsFormNetwork());
   }
 }
--- lib/domain/services/timetable_service.dart
+++ lib/domain/services/timetable_service.dart
@@ -2,10 +2,12 @@ import 'package:flutter_hive_json/domain/data/timetable_data.dart';
 import 'package:flutter_hive_json/domain/entity/timetable.dart';
 
 class TimetableService {
-  final timetableData = TimetableData();
-  Timetable get timetable => timetableData.timetable;
-  void initData() {
-    timetableData.load();
+  final _timetableData = TimetableData();
+  var _timetable = Timetable(posts: []);
+  Timetable get timetable => _timetableData.timetable;
+  Future<void> initData() async {
+    _timetable = await _timetableData.load();
+    // _timetable.toJson(); // <== нихуясе
   }
 
   TimetableService() {
